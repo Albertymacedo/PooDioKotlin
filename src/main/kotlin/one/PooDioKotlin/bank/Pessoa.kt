4dcda43b67ffba31f7ea25f8c69b0432de0d686e
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-08-26 16:18:10 -0300

    primeiro commit

---
+++ src/main/kotlin/one/PooDioKotlin/bank/Pessoa.kt
@@ -0,0 +1,11 @@
+package one.PooDioKotlin.bank
+
+class Pessoa {
+    var nome: String = "Alberty"
+    var cpf: String = "123.456.789.00"
+}
+ fun main(){
+     val eu = Pessoa()
+         println(eu.nome)
+        println(eu.cpf)
+ }
