c09be8232f905c09722f82770d681e8f2c979984
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-08-30 15:32:48 -0300

    segundo commit

---
+++ src/main/kotlin/one/PooDioKotlin/bank/Conta.kt
@@ -0,0 +1,16 @@
+package one.PooDioKotlin.bank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numeroo: String,
+    val saldo: BigDecimal
+)    {
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
--- src/main/kotlin/one/PooDioKotlin/bank/Pessoa.kt
+++ src/main/kotlin/one/PooDioKotlin/bank/Pessoa.kt
@@ -3,9 +3,16 @@ package one.PooDioKotlin.bank
 class Pessoa {
     var nome: String = "Alberty"
     var cpf: String = "123.456.789.00"
+    private set //não consigo alterar os valores das variáveis em outras classes (apenas get)
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
+
  fun main(){
      val eu = Pessoa()
-         println(eu.nome)
-        println(eu.cpf)
+         println(eu.pessoaInfo())
+
+
  }
