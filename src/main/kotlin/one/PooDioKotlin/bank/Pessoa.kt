f332045001fe771febad03683aa3bf6f80a70984
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-09-13 15:57:21 -0300

    created teste funcionario e funcionario extendendo de Pessoa

--- src/main/kotlin/one/PooDioKotlin/bank/Pessoa.kt
+++ src/main/kotlin/one/PooDioKotlin/bank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.PooDioKotlin.bank
 
-class Pessoa {
-    var nome: String = "Alberty"
-    var cpf: String = "123.456.789.00"
-    private set //não consigo alterar os valores das variáveis em outras classes (apenas get)
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
- fun main(){
-     val eu = Pessoa()
-         println(eu.pessoaInfo())
-
-
- }
+open class Pessoa (
+    open val nome: String,
+    open val cpf: String
+)
---
+++ src/main/kotlin/one/PooDioKotlin/bank/testes/TestePessoa.kt
@@ -0,0 +1,9 @@
+package one.PooDioKotlin.bank.testes
+
+import one.PooDioKotlin.bank.Pessoa
+
+fun main(){
+    val eu = Pessoa(nome = "Alberty Macedo", cpf = "1234567890")
+    println(eu.nome)
+    println(eu.cpf)
+}
